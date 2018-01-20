973822d81e0314d3d9b088c454e174688ac2a7e8
jepemo <Jeremias.Perez@desfici.com>
2018-01-20 10:12:49 +0100

    min changes

---
+++ install-or-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DF_DIR=.dotfiles_jepemo
+
+
+if [ -z "$1" ] || ([ "$1" != "ubuntu" ] && [ "$1" != "arch" ]); then
+    echo "You need to specify OS: <ubuntu> or <arch>"
+    exit 0
+fi
+
+
+OS_NAME=$1
+echo "> Installing for $OS_NAME"
+
+
+function create_dir {
+    rm -Rf $DF_DIR
+    mkdir $DF_DIR
+}
+
+
+function copy_common {
+    cp -R bin $DF_DIR/bin
+}
+
